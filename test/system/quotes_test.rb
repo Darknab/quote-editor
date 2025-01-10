03b8648323922ac5acc709004544be4d292126a7
Darknab <darknab@live.fr>
2025-01-10 20:09:09 +0100

    Update the tests to meet the SPA requirements

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -5,28 +5,56 @@ class QuotesTest < ApplicationSystemTestCase
     @quote = quotes(:first)
   end
 
-  test "Creating a new quote" do
-    # When we visit the Quotess#index page
-    # We expect to see a title with the text "Quotes"
-    visit quotes_path
-    assert_selector "h1", text: "Quotes"
+  #   test "Creating a new quote" do
+  #     # When we visit the Quotess#index page
+  #     # We expect to see a title with the text "Quotes"
+  #     visit quotes_path
+  #     assert_selector "h1", text: "Quotes"
 
-    # When we click on the link with the text "New quote"
-    # we expect to land on a page with the title "New quote"
-    click_on "New quote"
-    assert_selector "h1", text: "New quote"
+  #     # When we click on the link with the text "New quote"
+  #     # we expect to land on a page with the title "New quote"
+  #     click_on "New quote"
+  #     assert_selector "h1", text: "New quote"
 
-    # When we fill in the name input with "Capybara quote"
-    # and we click on the "Create Quote"
-    fill_in "Name", with: "Capybara quote"
-    click_on "Create quote"
+  #     # When we fill in the name input with "Capybara quote"
+  #     # and we click on the "Create Quote"
+  #     fill_in "Name", with: "Capybara quote"
+  #     click_on "Create quote"
 
-    # we expect to be back on the page with the title "Quotes"
-    # and to see our "Capybara quote" added to the list
-    assert_selector "h1", text: "Quotes"
-    assert_text "Capybara quote"
-  end
+  #     # we expect to be back on the page with the title "Quotes"
+  #     # and to see our "Capybara quote" added to the list
+  #     assert_selector "h1", text: "Quotes"
+  #     assert_text "Capybara quote"
+  #   end
 
+  #   test "Showing a quote" do
+  #     visit quotes_path
+  #     click_link @quote.name
+
+  #     assert_selector "h1", text: @quote.name
+  #   end
+
+  #   test "Updating a quote" do
+  #     visit quotes_path
+  #     assert_selector "h1", text: "Quotes"
+
+  #     click_on "Edit", match: :first
+  #     assert_selector "h1", text: "Edit quote"
+
+  #     fill_in "Name", with: "Updated quote"
+  #     click_on "Update quote"
+
+  #     assert_selector "h1", text: "Quotes"
+  #     assert_text "Updated quote"
+  #   end
+
+  #   test "Destroying a quote" do
+  #     visit quotes_path
+  #     assert_text @quote.name
+
+  #     click_on "Delete", match: :first
+  #     assert_no_text @quote.name
+  #   end
   test "Showing a quote" do
     visit quotes_path
     click_link @quote.name
@@ -34,14 +62,28 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: @quote.name
   end
 
+  test "Creating a new quote" do
+    visit quotes_path
+    assert_selector "h1", text: "Quotes"
+
+    click_on "New quote"
+    fill_in "Name", with: "Capybara quote"
+
+    assert_selector "h1", text: "Quotes"
+    click_on "Create quote"
+
+    assert_selector "h1", text: "Quotes"
+    assert_text "Capybara quote"
+  end
+
   test "Updating a quote" do
     visit quotes_path
     assert_selector "h1", text: "Quotes"
 
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit quote"
-
     fill_in "Name", with: "Updated quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Update quote"
 
     assert_selector "h1", text: "Quotes"
